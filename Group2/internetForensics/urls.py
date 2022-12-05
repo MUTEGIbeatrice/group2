57c393dba042eef879fd95f66f572fb6997b3e55
MUTEGIbeatrice <77670352+MUTEGIbeatrice@users.noreply.github.com>
2022-12-05 17:17:10 +0100

    templates added

--- Group2/internetForensics/urls.py
+++ Group2/internetForensics/urls.py
@@ -4,8 +4,8 @@ from . import views #importing from views.py file
 #importing the url path (html responses) from views.py file 
 urlpatterns = [
 
-    path('', views.home),
-    path('register/',views.register),
+    path('', views.login),
+    path('login/',views.login),
     path('dashboard/',views.dashboard),
     path('reports/',views.reports),
     path('criminalactivity/',views.criminalActivity),
--- Group2/internetForensics/views.py
+++ Group2/internetForensics/views.py
@@ -6,23 +6,20 @@ from django.http import HttpResponse
 
 # Create your views here.
  
-def home(request):
-    return HttpResponse('Home page')
-    
-def register(request):
-    return HttpResponse('Registration page')
+def login(request):
+    return HttpResponse('Login page')
 
 def dashboard(request):
     return HttpResponse('Dashboard page')
     
-def reports(request):
+def reports(request): #report management
     return HttpResponse('Reports page')
 
-def criminalActivity(request):
+def criminalActivity(request): #search and locate criminal activities
     return HttpResponse('Criminal Activity page')
 
-def audits(request):
+def audits(request): #manage audits
     return HttpResponse('Audits page')   
 
-def cases(request):
+def cases(request): #manage cases
     return HttpResponse('Cases page')
