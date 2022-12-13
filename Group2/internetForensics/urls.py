4e6479e20747009e95446a6ff294c52a4d72788b
MUTEGIbeatrice <77670352+MUTEGIbeatrice@users.noreply.github.com>
2022-12-13 12:35:04 +0100

    edit

--- Group2/internetForensics/urls.py
+++ Group2/internetForensics/urls.py
@@ -4,11 +4,12 @@ from . import views #importing from views.py file
 #importing the url path from views.py file 
 urlpatterns = [
 
-    path('', views.index),
-    path('index/',views.index),
-    path('mainpage/',views.mainpage),
-    path('reports/',views.reports),
-    path('criminalactivity/',views.criminalActivity),
-    path('audits/',views.audits),
-    path('cases/',views.cases),
+    path('', views.index, name="index"),
+    path('index/',views.index, name="index"),
+    path('mainpage/',views.mainpage, name="mainpage"),
+    path('reports/',views.reports, name="reports"),
+    path('criminalactivity/',views.criminalActivity, name="criminalactivity"),
+    path('audits/',views.audits, name="audits"),
+    path('cases/',views.cases, name="cases"),
+    
 ]
--- Group2/internetForensics/views.py
+++ Group2/internetForensics/views.py
@@ -1,12 +1,34 @@
 from django.shortcuts import render, redirect
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout #FOR AUTH
 
 # Create your views here.
 
- 
-def index(request):
-    return render(request, 'internetForensics/index.html')
+ # LOGIN  REQUEST AND AUTH 
 
+def index(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+   
+    if user is not None:
+        login(request, user)
+        return redirect('mainpage')
+    else:
+        messages.info(request, 'Username or Password is Incorrect')
+
+    context = {}
+    return render(request, 'internetForensics/index.html', context)
+
+
+# LOG OUT
+def logoutUser(request):
+    return redirect('index')
+
+# REQUESTS FOR THE REST OF THE PAGES 
 def mainpage(request):
     return render(request, 'internetForensics/mainpage.html')
     
@@ -21,3 +43,4 @@ def audits(request): #manage audits
 
 def cases(request): #manage cases
     return render(request, 'internetForensics/cases.html')
+
